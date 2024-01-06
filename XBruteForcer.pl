1e1be0b66cf508e591df326aeab32a7813c58e17
methnani ali <150056524+ai040989@users.noreply.github.com>
2024-01-06 23:39:49 +0100

    Update XBruteForcer.pl

--- XBruteForcer.pl
+++ XBruteForcer.pl
@@ -42,7 +42,7 @@ print color('bold red')," /_/ \\_\\ ";
 print color('bold white')," |____/|_|   \\__,_|\\__\\___|  |_|  \\___/|_|  \\___\\___|_|  ";   
 print color('bold red'),"v1.1\n\n";
 print color('bold red'),"\t\t        [";
-print color('bold white'),"Coded BY Mohamed Riahi";
+print color('bold white'),"Coded BY XBruteForcer";
 print color('bold red'),"]\n";
 print color('reset');
 };
